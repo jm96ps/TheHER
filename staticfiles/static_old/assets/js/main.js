671caa461cc7f9a190641192c2758138042c92a4
James Mário <jamesmps021@gmail.com>
2026-01-31 16:02:07 -0300

    chore: remove legacy Flask app and static_old

--- staticfiles/static_old/assets/js/main.js
+++
@@ -1,6 +0,0 @@
-// static_old proxy loader for Prologue main.js
-(function(){
-  var s = document.createElement('script');
-  s.src = '/static/html5up-prologue/assets/js/main.js';
-  document.head.appendChild(s);
-})();
